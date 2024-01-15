cc6027212641c209925919f4f0fa9706aad38f7d
wltwt <wltwt@proton.me>
2024-01-15 12:52:49 +0100

    add htmldata

--- ov1/simple_server/src/main.cpp
+++ ov1/simple_server/src/main.cpp
@@ -1,16 +1,28 @@
 #include <Arduino.h>
-
+#include <AsyncTCP.h>
 #include <WiFi.h>
+#include <ESPAsyncWebServer.h>
 
 const char* ssid = "NTNU-IOT";
 const char* password = "";
 
 
 
+AsyncWebServer server(80);
 
-
-// put function declarations here:
 void connectWiFi();
+
+String HTMLdata = "<!DOCTYPE html>"
+  "<html>"
+  "<head>"
+  "<title>Sensordata</title>"
+  "<meta http-equiv='refresh' content='0.5'>"
+  "</head>"
+  "<body>"
+  "<h1>Sensordata</h1>"
+  "</body>"
+  "</html>";
+
 
 void setup() {
   Serial.begin(115200);
@@ -24,11 +36,7 @@ void loop() {
     connectWiFi();
   }
 
-
-
-
-
-
+  
 }
 
 
